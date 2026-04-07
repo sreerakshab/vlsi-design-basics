49b2788c0c5ac1f1a29d24f5be960f0f148731ac
sreerakshab <76564610+sreerakshab@users.noreply.github.com>
2026-04-07 20:25:05 +0530

    Added Mealy FSM 101 sequence detector

---
+++ mealy_fsm/tb/mealyseq_tb.sv
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.04.2026 20:04:21
+// Design Name: 
+// Module Name: mealyseq_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+module mealyseq_tb;
+
+    reg reset;
+    reg clk;
+    reg in_seq;
+    wire out_detect;
+
+    mealyseq dut(
+        .reset(reset),
+        .clk(clk),
+        .in_seq(in_seq),
+        .out_detect(out_detect)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        $display("Time\t reset in_seq out_detect");
+        $monitor("%0t\t %b    %b      %b", $time, reset, in_seq, out_detect);
+
+        clk = 0;
+        reset = 1;
+        in_seq = 1;
+
+        #10;
+        reset = 0;
+
+        #10 in_seq = 0;
+        #10 in_seq = 1; 
+        #10 in_seq = 0;
+        #10 in_seq = 1; 
+        #10 in_seq = 1;
+        #10 in_seq = 0;
+        #10 in_seq = 1; 
+
+        #20 $finish;
+    end
+
+endmodule
